576c301bcee171ace4e6e02a8419e76fb91dcb93
Roman Roman <45037050-filigo4900@users.noreply.replit.com>
2025-07-15 00:16:32 +0000

    Initial commit from Replit

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'drizzle-kit';
+import dotenv from "dotenv";
+dotenv.config();
 
-export default defineConfig({
-  schema: './schema.ts', // укажи здесь файл со схемой таблиц (описан на TypeScript)
-  out: './drizzle', // куда будут сохраняться миграции
-  driver: 'pg',  // драйвер Postgres
+import { config } from "drizzle-kit";
+
+export default {
+  schema: "./server/schema.ts",  // путь к твоей схеме
+  out: "./migrations",
+  driver: "pg",
+  dialect: "postgresql",   // вот сюда обязательно укажи dialect
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL, // укажи переменную окружения с твоей БД
+    connectionString: process.env.DATABASE_URL,
   },
-});
+} satisfies config;
